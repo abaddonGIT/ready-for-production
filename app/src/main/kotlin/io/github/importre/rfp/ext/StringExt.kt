12fe97a616601c8bc230a8d01519cf3ff87ece07
Jaewe Heo <jaeweheo@gmail.com>
2016-03-08 21:33:11 +0900

    cleanup

--- app/src/main/kotlin/io/github/importre/rfp/ext/StringExt.kt
+++ app/src/main/kotlin/io/github/importre/rfp/ext/StringExt.kt
@@ -1,4 +1,3 @@
 package io.github.importre.rfp.ext
 
-fun String.join(vararg strings: String): String = strings.joinToString(this)
-
+fun String.join(vararg strings: String): String = strings.joinToString(separator = this)
