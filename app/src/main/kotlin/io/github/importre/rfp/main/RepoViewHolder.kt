9ab65e7ff877833442b2d894a0d7b44a33c3ddba
Jaewe Heo <jaeweheo@gmail.com>
2016-03-09 23:19:10 +0900

    init card view on view holder by using android extension

--- app/src/main/kotlin/io/github/importre/rfp/main/RepoViewHolder.kt
+++ app/src/main/kotlin/io/github/importre/rfp/main/RepoViewHolder.kt
@@ -3,28 +3,25 @@ package io.github.importre.rfp.main
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.Toast
-import io.github.importre.rfp.R
 import io.github.importre.rfp.api.repo.Repository
-import io.github.importre.rfp.ext.getTagAsImageView
-import io.github.importre.rfp.ext.getTagAsTextView
 import io.github.importre.rfp.ext.join
 import io.github.importre.rfp.ext.loadUrl
+import kotlinx.android.synthetic.main.layout_repo_card.view.*
 
 class RepoViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
 
     fun setRepo(repo: Repository) {
-        view.run {
-            findViewById(R.id.fg).setOnClickListener {
+        itemView.run {
+            fg.setOnClickListener {
                 val message = repo.run {
                     "\n".join(fullName, language)
                 }
                 Toast.makeText(view.context, message, Toast.LENGTH_SHORT).show()
             }
-
-            getTagAsImageView(R.id.ownerImage).loadUrl(repo.owner.avatarUrl)
-            getTagAsTextView(R.id.name).text = repo.name
-            getTagAsTextView(R.id.star).text = "${repo.stargazersCount}"
-            getTagAsTextView(R.id.fork).text = "${repo.forksCount}"
+            ownerImage.loadUrl(repo.owner.avatarUrl)
+            name.text = repo.name
+            star.text = "${repo.stargazersCount}"
+            fork.text = "${repo.forksCount}"
         }
     }
 }
